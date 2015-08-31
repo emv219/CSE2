11a3839281c754f9940c189529548cf03966779c
vailevan <emv219@lehigh.edu>
2015-08-31 21:02:56 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+//Evan Vail
+//8-30-15
+//CSE2
+//Section 110
+//Welcome Class Java Program 
+//  First compile the program 
+//      javac WelcomeClass.java
+//  Run the program     
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+//print the statements 
+    System.out.println("---------------------");
+    System.out.println("|      Welcome      |");
+    System.out.println("---------------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-E--M--V--2--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println(" My name is Evan and i like to play tennis and chess");
+ 
+    }
+}
